be8aafb3f4863397e926a61dfd92bb682c5d81ea
Beknur <106554639+BEknUur@users.noreply.github.com>
2024-10-24 20:41:24 +0500

    third task

---
+++ back-end-101/lesson-3/utils/helpers.py
@@ -0,0 +1,24 @@
+def brush_teeth():
+    print("Brush the teets")
+
+def make_coffee():
+    print("Drink a coffee")
+
+def do_exercise():
+    print("Do a morning exercises")
+
+def take_shower():
+    print("Take the showers")
+
+def eat_breakfast():
+    print("Eat the breakfast")
+
+
+
+def lesgo():
+    brush_teeth()
+    make_coffee()
+    do_exercise()
+    take_shower()
+    eat_breakfast()
+    
